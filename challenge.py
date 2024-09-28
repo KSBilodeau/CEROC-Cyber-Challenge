0acb08eebbf1549546a118fb969dba0e51634544
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-09-28 13:59:58 -0500

    Database (#22)
    * Revert "Resolve syntax error in #2 (#7)"
    
    This reverts commit e5a1759cc05fba51097a58f815cfa6a4d0b19d54.
    
    revert: Regression in logging
    
    Undoes the commit that modified line 8 that handles the logging entry point for the bot.
    
    * fix: Change subclass to correct class reference
    
    Changes the MyClient object's subclass from the invalid `discord.bot` to `commands.bot`.
    
    * fix: Reapply commit e5a1759 with the correct logging entry point
    
    Change all references to client to bot in accordance with commit e5a1759 with the appropriate logging entry point.
    
    * refactor: Change logging capitalization
    
    Change `discord.Bot` to `discord.bot` to match with other existing logging entry points.
    
    * feat: Add framework for front end work
    
    Creates a basic extension that acts a skeleton for the front end of the bot. Contributes to resolution of issue #4.
    
    * feat: Add firebase admin to project
    
    Adds the firebase-admin library to the project for interfacing to the database.
    
    * mass commit, DO NOT PUSH UNDER ANY CIRCUMSTANCES
    
    * Chore: Revert to command skeleton
    
    Sets the commands back into a skeletal state to allow the rest of the team to work on each.
    
    * Fix: Remove message permissions
    
    Removes the message permissions and by proxy the command prefix for the bot.  This ensures that we do not ever have to get extra validation for our bot to function.
    
    * Move repository in new direction

--- challenge.py
+++ challenge.py
@@ -12,35 +12,42 @@ class Challenge(commands.Cog):
 
     @group.command()
     async def search(self, interaction: discord.Interaction, query: str):
+        # Necessary for all commands, ensures the message is only available to the user
         await interaction.response.defer(ephemeral=True)
-        await interaction.followup.send(f'Your query was "{query}"!')
+        # Dummy embed created as a starting point and test
+        embed = discord.Embed(title="Challenge Search", color=0x4e5d94, description=f'Your query was "{query}"!')
+        # Necessary to complete an interaction, as it sends the embed
+        await interaction.followup.send(embed=embed)
 
     @group.command()
     async def list(self, interaction: discord.Interaction):
+        # Necessary for all commands, ensures the message is only available to the user
         await interaction.response.defer(ephemeral=True)
-        await interaction.followup.send(f'There are no available challenges yet!')
+        # Dummy embed created as a starting point and test
+        embed = discord.Embed(title="Challenge List", color=0x4e5d94)
+        # Necessary to complete an interaction, as it sends the embed
+        await interaction.followup.send(embed=embed)
 
     @group.command()
-    async def submit(self, interaction: discord.Interaction, challenge_id: int, string: str = None,
-                     number: int = None, file: discord.Attachment = None):
+    async def submit(self, interaction: discord.Interaction, challenge_id: int, flag: str):
+        # Necessary for all commands, ensures the message is only available to the user
         await interaction.response.defer(ephemeral=True)
-        message = f'Receipt of submission for Challenge #{int(challenge_id)}:\n```'
+        # Not all followups are embeds!  You can also send strings.
+        await interaction.followup.send(f'You submitted:\nChallenge ID: {challenge_id}\nChallenge Flag: {flag}')
 
-        if string is not None:
-            message += f'Str Flag:\n\t"{string}"\n'
+    @group.command()
+    async def add(self, interaction: discord.Interaction):
+        # This function should be sending a modal, but for the sake of the skeleton, it'll defer
+        await interaction.response.defer(ephemeral=True)
+        # Dummy response as this function would normally send a modal
+        await interaction.followup.send("THIS IS A TEST")
 
-        if number is not None:
-            message += f'Num Flag:\n\t{int(number)}\n'
-
-        if file is not None:
-            message += "File Flag:\n"
-            message += f'\tName: {file.filename}\n'
-            message += f'\tSize: {file.size}\n'
-            message += f'\tDesc: "{file.description}"\n'
-
-        message += "```"
-
-        await interaction.followup.send(message)
+    @group.command()
+    async def end(self, interaction: discord.Interaction, challenge_id: int):
+        # Necessary for all commands, ensures the message is only available to the user
+        await interaction.response.defer(ephemeral=True)
+        # Not all followups are embeds!  You can also send strings.
+        await interaction.followup.send(f'You have asked to end Challenge #{challenge_id}')
 
 
 async def setup(bot: commands.Bot):
@@ -48,5 +55,5 @@ async def setup(bot: commands.Bot):
     logger.info("Extension loaded successfully!")
 
 
-async def teardown(bot):
+async def teardown(_: commands.Bot):
     logger.info("Extension unloaded successfully!")
--- main.py
+++ main.py
@@ -3,24 +3,23 @@ import logging
 
 from discord.ext import commands
 
-
 class MyBot(commands.Bot):
     # Retrieve the logging object for the client.
     logger = logging.getLogger('discord.bot')
 
-    async def on_ready(self):
-        # Load the extension
+    async def setup_hook(self):
+        # Load the command extension/s before the bot starts
         await self.load_extension('challenge')
 
+    async def on_ready(self):
         # Note the bot successfully logged in and is ready to work.
         self.logger.info(f'Logged is as {self.user}!')
 
 
 # Create an intents object and enable the MSG_CONTENT intent for the on_message event
 intents = discord.Intents.default()
-intents.message_content = True
 
 # Construct an instance of the Client with the given intents.
-bot = MyBot(intents=intents, command_prefix="nibble ")
+bot = MyBot(intents=intents, command_prefix=[])
 # Request console input for the bot token and pass it to the client for it to initialize
 bot.run(input('Enter a bot token: '))
